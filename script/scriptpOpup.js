7aa8608858b51a05923c940084324c6f6a2708d5
sherbel-ilabouni <shilabuni@gmail.com>
2024-11-26 14:07:19 +0200

    new4

--- script/scriptpOpup.js
+++ script/scriptpOpup.js
@@ -1,7 +1,15 @@
 setTimeout(function () {
+  document.querySelector(".overlay").style.display = "block";
   document.getElementById("popup-contact").style.display = "block";
-}, 15000); // הצגת הטופס לאחר 15 שניות
+}, 15000);
 
 document.getElementById("close-popup").addEventListener("click", function () {
+  document.querySelector(".overlay").style.display = "none";
+  document.getElementById("popup-contact").style.display = "none";
+});
+
+// סגירת הפופאפ בלחיצה על המסך הכהה
+document.querySelector(".overlay").addEventListener("click", function () {
+  document.querySelector(".overlay").style.display = "none";
   document.getElementById("popup-contact").style.display = "none";
 });
